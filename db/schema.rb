d59b5299c8c2a53f185e1dd83a6d3e36d24b9369
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-21 13:52:11 -0300

    Adding max points to exercise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722003532) do
+ActiveRecord::Schema.define(version: 20150721162612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20150722003532) do
     t.text     "corollary"
     t.integer  "layout",            default: 0,    null: false
     t.text     "expectations"
+    t.integer  "max_points",        default: 10
   end
 
   add_index "exercises", ["author_id"], name: "index_exercises_on_author_id", using: :btree
